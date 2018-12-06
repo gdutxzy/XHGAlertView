7068cf1171e3c2a4bee5ecd6cd727d3ce73fef64
XZY <xiezongyuan@xhg.com>
2018-12-06 16:56:28 +0800

    UIbutton 分类函数改名

--- XHGAlertView/Classes/UIButton+TouchUpInsideBlock.m
+++ XHGAlertView/Classes/UIButton+TouchUpInsideBlock.m
@@ -14,8 +14,8 @@
 //动态绑定
 static char *xhg_actionBlock_bind;
 - (void)setXhg_clickBlock:(void (^)(UIButton *))actionBlock {
-    [self removeTarget:self action:@selector(UIButtonActionBlockClick:) forControlEvents:UIControlEventTouchUpInside];
-    [self addTarget:self action:@selector(UIButtonActionBlockClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self removeTarget:self action:@selector(xhg_touchUpInsideAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:self action:@selector(xhg_touchUpInsideAction:) forControlEvents:UIControlEventTouchUpInside];
     objc_setAssociatedObject(self, &xhg_actionBlock_bind, actionBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
 
@@ -25,7 +25,7 @@ static char *xhg_actionBlock_bind;
 
 
 //事件响应
-- (void)UIButtonActionBlockClick:(UIButton *)button {
+- (void)xhg_touchUpInsideAction:(UIButton *)button {
     !self.xhg_clickBlock ? nil : self.xhg_clickBlock(self);
 }
 @end
